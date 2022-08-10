f509b515c7fa76ef00bd5ddc5b23c560e71b314d
cryptoKris <github@knowhere.be>
2022-08-10 16:05:58 +0200

    write the smc for the rsvp dapp

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 contract Web3RSVP {
-    
     struct CreateEvent {
         bytes32 eventId;
         string eventDataCID; //a reference to an IPFS to store event name and description
@@ -24,12 +23,15 @@ contract Web3RSVP {
         string calldata eventDataCID
     ) external {
         //generate an eventId based on other things passed in => hash
-        bytes32 eventId = keccak256(abi.encodePacked(
-            msg.sender,
-            address(this),
-            eventTimestamp,
-            deposit,
-            maxCapacity));
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
 
         address[] memory confirmedRSVPs; //initialize the array to be used in the struct
         address[] memory claimedRSVPs;
@@ -56,20 +58,124 @@ contract Web3RSVP {
         require(msg.value == myEvent.deposit, "NOT ENOUGH token provided");
 
         //require that the event hasn't started yet
-        require(block.timestamp <= myEvent.eventTimestamp, "The event has already passed");
+        require(
+            block.timestamp <= myEvent.eventTimestamp,
+            "The event has already passed"
+        );
 
         //make sure the event is under the maxCapacity
         require(
-            myEvent.confirmedRSVPs.length < myEvent.maxCapacity, 
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
             "This event has reached its max capacity"
         );
 
         //require that the msg.sender isn't already in the list of confirmedRSVPs
-        for(uint8 i=0; i < myEvent.confirmedRSVPs.length; i++){
-            require(myEvent.confirmedRSVPs[i] != msg.sender, "Already confirmed");
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(
+                myEvent.confirmedRSVPs[i] != msg.sender,
+                "Already confirmed"
+            );
         }
 
         myEvent.confirmedRSVPs.push(payable(msg.sender));
     }
 
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        // look up event from our struct using the eventId
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        // require that msg.sender is the owner of the event - only the host should be able to check people in
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        // require that attendee trying to check in actually RSVP'd
+        address rsvpConfirm;
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            if (myEvent.confirmedRSVPs[i] == attendee) {
+                rsvpConfirm = myEvent.confirmedRSVPs[i];
+            }
+        }
+
+        require(rsvpConfirm == attendee, "NO RSVP TO CONFIRM");
+
+        // require that attendee is NOT already in the claimedRSVPs list AKA make sure they haven't already checked in
+        for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
+        }
+
+        // require that deposits are not already claimed by the event owner
+        require(myEvent.paidOut == false, "ALREADY PAID OUT");
+
+        // add the attendee to the claimedRSVPs list
+        myEvent.claimedRSVPs.push(attendee);
+
+        // sending eth back to the staker `https://solidity-by-example.org/sending-ether`
+        /**
+        call in combination with re-entrancy guard is the recommended method to use after December 2019.
+
+        Guard against re-entrancy by
+
+            making all state changes before calling other contracts
+            using re-entrancy guard modifier
+        */
+
+        //recommended method is call plus making all state changes before calling other contract and re-entrancy guard
+        (bool sent, ) = attendee.call{value: myEvent.deposit}("");
+
+        // if this fails, remove the user from the array of claimed RSVPs
+        if (!sent) {
+            myEvent.claimedRSVPs.pop();
+        }
+        //prevent re-entry
+        require(sent, "Failed to send Ether");
+    }
+
+    function confirmAllAttendees(bytes32 eventId) external {
+        // look up event from our struct with the eventId
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        // make sure you require that msg.sender is the owner of the event
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        // confirm each attendee in the rsvp array
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    function withdrawUnclaimedDeposits(bytes32 eventId) external {
+        // look up event
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        // check that the paidOut boolean still equals false AKA the money hasn't already been paid out
+        require(!myEvent.paidOut, "ALREADY PAID");
+
+        // check if it's been 7 days past myEvent.eventTimestamp
+        require(
+            block.timestamp >= (myEvent.eventTimestamp + 7 days),
+            "TOO EARLY"
+        );
+
+        // only the event owner can withdraw
+        require(msg.sender == myEvent.eventOwner, "MUST BE EVENT OWNER");
+
+        // calculate how many people didn't claim by comparing
+        uint256 unclaimed = myEvent.confirmedRSVPs.length -
+            myEvent.claimedRSVPs.length;
+
+        uint256 payout = unclaimed * myEvent.deposit;
+
+        // mark as paid before sending to avoid reentrancy attack
+        myEvent.paidOut = true;
+
+        // send the payout to the owner
+        (bool sent, ) = msg.sender.call{value: payout}("");
+
+        // if this fails
+        if (!sent) {
+            myEvent.paidOut == false;
+        }
+
+        require(sent, "Failed to send Ether");
+    }
 }
